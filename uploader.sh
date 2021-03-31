3ab14dacc36244000e230cd89a9ac4d1ebabfcd4
root <root@localhost.localdomain>
2021-03-31 17:59:53 +0000

    BACKUP

---
+++ uploader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+host='host'
+user='usuario'
+pass='Password'
+remoth='/'
+now="$(date +'%d-%m-%Y')"
+zip -r -Z bzip2 "$now.zip" Ruta_backup
+file="$now.zip"
+sftp $user@$host <<SOMEDELIMITER
+	quote $pass
+	put $file
+	quit
+SOMEDELIMITER
+exit 0
